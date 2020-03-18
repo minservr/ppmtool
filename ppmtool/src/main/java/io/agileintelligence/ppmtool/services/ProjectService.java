364353c84e2ad9deec20640950f9a79a7dd289c1
minservr <a.kaaoua@anasys.fr>
2020-03-18 12:39:37 +0100

    branch8

--- ppmtool/src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
+++ ppmtool/src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
@@ -17,8 +17,16 @@ public class ProjectService {
             project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
             return projectRepository.save(project);
         } catch (Exception e) {
-            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already  exists");
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists");
         }
 
+    }
+
+    public Project findProjectByIdentifier (String projectId) {
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+        if (project == null) {
+            throw new ProjectIdException("Project ID '" + projectId.toUpperCase() + "' doesn't exist");
+        }
+        return project;
     }
 }
